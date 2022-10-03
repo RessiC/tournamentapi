7ff295269b189d7e1a7a5efd4356e764a6b78199
ressic <ressi.cyril@gmail.com>
2022-10-03 18:29:49 +0200

    feat: team

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity\User;
 
-use App\Entity\Team;
+use App\Entity\Team\Team;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
