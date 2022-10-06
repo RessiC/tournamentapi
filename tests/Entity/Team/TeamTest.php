0bbb1ceb5fab0a2eb8c00172d7038c6b0723df26
ressic <ressi.cyril@gmail.com>
2022-10-06 10:41:41 +0200

    test: Team entity

--- tests/Entity/Team/TeamTest.php
+++ tests/Entity/Team/TeamTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Team\Team;
+use App\Entity\User\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TeamTest extends KernelTestCase
@@ -15,5 +16,28 @@ class TeamTest extends KernelTestCase
         $this->assertInstanceOf(Team::class, $team);
         $this->assertNotNull($team->getPlayers());
 
+    }
+
+    public function testAddPlayers()
+    {
+        $team = new Team();
+        $player1 = new User();
+        $player2= new User();
+        $team->addPlayer($player1);
+        $team->addPlayer($player2);
+
+        $this->assertNotNull($team->getPlayers());
+        $this->assertInstanceOf(Team::class, $team);
+        $this->assertContainsOnlyInstancesOf(User::class, $team->getPlayers());
+    }
+
+    public function testRemovePlayer()
+    {
+        $team = new Team();
+        $player1 = new User();
+        $team->addPlayer($player1);
+        $team->removePlayer($player1);
+
+        $this->assertCount(0, $team->getPlayers());
     }
 }
